d3ee2828c2ce853ad4f54e65682936a86a22efaa
HiromiShikata <shikata.hiromi@gmail.com>
2021-11-22 03:06:17 +0000

    fix: bug of calculation of date

--- src/adapters/gateways/repositoeirs/SystemDatetimeRepository.ts
+++ src/adapters/gateways/repositoeirs/SystemDatetimeRepository.ts
@@ -2,7 +2,7 @@ import {DatetimeRepository} from '../../../usecases/MoveCardsByDateTimeUsecase'
 
 export class SystemDatetimeRepository implements DatetimeRepository {
   dateBeforeDays = (days: number): Date =>
-    new Date(this.now().getTime() + 1000 * 60 * 60 * 24 * days)
+    new Date(this.now().getTime() - 1000 * 60 * 60 * 24 * days)
 
   now = (): Date => new Date()
 }
