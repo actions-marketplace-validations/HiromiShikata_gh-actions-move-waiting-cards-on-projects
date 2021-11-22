5d20e2791c42afa5f415b537defa90ce6e73aec4
HiromiShikata <shikata.hiromi@gmail.com>
2021-11-22 03:22:18 +0000

    test: ad ConsumerExpectationTest

---
+++ __tests__/ConsumerExpectation.test.ts
@@ -0,0 +1,35 @@
+import {MoveCardsByDateTimeUsecase} from '../src/usecases/MoveCardsByDateTimeUsecase'
+import {SystemDatetimeRepository} from '../src/adapters/gateways/repositoeirs/SystemDatetimeRepository'
+import {OctokitGithubRepository} from '../src/adapters/gateways/repositoeirs/OctokitGithubRepository'
+
+describe('ConsumerExpectation Test', () => {
+  test('execute', async () => {
+    const githubRepo = new OctokitGithubRepository(
+      'HiromiShikata',
+      'gh-actions-move-waiting-cards-on-projects',
+      process.env.GH_TOKEN || '',
+      5,
+      15,
+      3
+    )
+    const usecase = new MoveCardsByDateTimeUsecase(
+      new SystemDatetimeRepository(),
+      githubRepo,
+      {
+        show: (log: string) => {
+          console.log(log)
+        }
+      }
+    )
+    await usecase.execute(
+      'test-project',
+      'In' + ' waiting',
+      'To do',
+      'waiting till DATETIME/',
+      [],
+      20
+    )
+    const todoCards = await githubRepo.getCards('test-project', 'To do')
+    await githubRepo.moveCard(todoCards[0], 'test-project', 'In waiting')
+  })
+})
